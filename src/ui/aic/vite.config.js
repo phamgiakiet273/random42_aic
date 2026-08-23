254ab402b04816421c5f5d6f6f359dd4fb66fcad
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2026-08-23 21:42:34 +0700

    feat: add host for cloudflare frontend

--- src/ui/aic/vite.config.js
+++ src/ui/aic/vite.config.js
@@ -10,4 +10,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["aic_fe.serverhub.id.vn"]
+  }
+  
 })
